9da123526e6833573c3ed50d8c902311bdc1175a
EmperorOfCatkind <kirillaleksandrov84@gmail.com>
2024-10-18 16:19:47 +0200

    Unit Prototype Basics added

--- Assets/Scripts/Units/Prototype.cs
+++ Assets/Scripts/Units/Prototype.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Prototype : MonoBehaviour
 {
     private Vector3 targetPosition;
     private float moveSpeed = 5f;
-    private float rotationSpeed = 10f;
+    private float rotationSpeed = 15f;
     private float stoppingDistance = 0.1f;
+    private float rotatingStoppingDistance = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,14 +23,21 @@ public class Prototype : MonoBehaviour
         if(Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
         {
             
-            Vector3 moveDirection = (targetPosition - transform.position).normalized;
+            /*Vector3 moveDirection = (targetPosition - transform.position).normalized;
 
             
             transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotationSpeed);
             
             
-            transform.position += moveDirection * moveSpeed * Time.deltaTime;
+            transform.position += moveDirection * moveSpeed * Time.deltaTime;*/
+
+            RotateToMoveDirection();
+            if(Vector3.Distance(transform.forward, GetMoveDirection()) < rotatingStoppingDistance)
+            {
+                GoForward();
+            }
         }
+
 
         if(Input.GetMouseButtonDown(0))
         {
@@ -46,5 +55,21 @@ public class Prototype : MonoBehaviour
     private void Move(Vector3 targetPosition)
     {
         this.targetPosition = targetPosition;
+    }
+
+    private Vector3 GetMoveDirection()
+    {
+        Vector3 moveDirection = (targetPosition - transform.position).normalized;
+        return moveDirection; 
+    }
+
+    private void GoForward()
+    {
+        transform.position += GetMoveDirection() * moveSpeed * Time.deltaTime;
+    }
+
+    private void RotateToMoveDirection()
+    {
+        transform.forward = Vector3.Lerp(transform.forward, GetMoveDirection(), Time.deltaTime * rotationSpeed);
     }
 }
